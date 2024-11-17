48fe6bfa531ee24637660d37c54038ba39afc7d5
양동진 <dong3831@hanmail.net>
2024-11-17 21:48:59 +0900

    chapter 9 - nat gateway

--- chapter8/vpc.tf
+++ chapter8/vpc.tf
@@ -27,3 +27,47 @@ resource "aws_subnet" "second_subnet" {
 		Name = "101subnet-2"
 	}
 }
+
+resource "aws_internet_gateway" "igw" {
+	vpc_id = aws_vpc.main.id
+
+	tags = {
+		Name = "main"
+	}
+}
+
+resource "aws_eip" "nat_1" {
+	domain = "vpc"	
+
+	lifecycle {
+		create_before_destroy = true
+	}
+}
+
+resource "aws_eip" "nat_2" {
+	domain = "vpc"
+	
+	lifecycle {
+		create_before_destroy = true
+	}
+}
+
+resource "aws_nat_gateway" "nat_gateway_1" {
+	allocation_id = aws_eip.nat_1.id
+
+	subnet_id = aws_subnet.first_subnet.id
+
+	tags = {
+		Name = "NAT-GW-1"
+	}
+}
+
+resource "aws_nat_gateway" "nat_gateway_2" {
+	allocation_id = aws_eip.nat_2.id
+
+	subnet_id = aws_subnet.second_subnet.id
+
+	tags = {
+		Name = "NAT-GW-2"
+	}
+}
